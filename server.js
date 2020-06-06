707ea4d17915e5348ce702bd9fa20c681c4667bd
DajakStipe <dajakstipe@gmail.com>
2020-06-06 19:44:13 +0200

    Added routes for bootcamp.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Route Files -- importing the file
+const bootcamps = require('./routes/bootcamps');
 
 // load ENV VARS
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routers -- define the route from the file
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-	)
+  PORT,
+  // eslint-disable-next-line no-console
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
