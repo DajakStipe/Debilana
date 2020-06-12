d8f5501b344e5aeed07e86d302e588063a3c8d12
DajakStipe <dajakstipe@gmail.com>
2020-06-12 20:34:36 +0200

    Installed Colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // load ENV VARS
@@ -25,12 +26,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   // eslint-disable-next-line no-console
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // handle unhandled rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error ${err.message}`);
+  console.log(`Error ${err.message}`.red);
 
   //close server & exit process, 1=failure
   server.close(() => process.exit(1));
