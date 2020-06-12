770f7a210aae11092d0514759706348f814662ec
DajakStipe <dajakstipe@gmail.com>
2020-06-12 20:30:12 +0200

    Added MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route Files -- importing the file
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load ENV VARS
 dotenv.config({ path: './config/config.env' });
+// connect to database
+connectDB();
 
+// Route Files -- importing the file
+const bootcamps = require('./routes/bootcamps');
 const app = express();
 
 //Dev logging middleware for morgan
@@ -20,8 +22,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   // eslint-disable-next-line no-console
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err.message}`);
+
+  //close server & exit process, 1=failure
+  server.close(() => process.exit(1));
+});
