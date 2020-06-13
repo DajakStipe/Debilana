bc34d6ae4bbe549cbd63f6b95fce0562f48b662c
DajakStipe <dajakstipe@gmail.com>
2020-06-13 18:05:55 +0200

    Added basic CRUD for Bootcamps

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const app = express();
 
+// Body Parser - so we dont get undefined
+app.use(express.json());
+
 //Dev logging middleware for morgan
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
