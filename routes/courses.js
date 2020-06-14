c2f5cf6a97792b6dee929d5ac3f5e3961a31e730
DajakStipe <dajakstipe@gmail.com>
2020-06-14 17:18:55 +0200

    Update and Delete Course

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 // importing methods from controller
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
