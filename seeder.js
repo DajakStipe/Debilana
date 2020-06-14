8a8d776a4142609f5fde432c7d3faffc3da734c5
DajakStipe <dajakstipe@gmail.com>
2020-06-14 14:32:00 +0200

    Added Courses in seeder.js for importing/deleting

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load Models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -24,10 +25,16 @@ const bootcamps = JSON.parse(
   'utf-8'
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`),
+  'utf-8'
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -39,6 +46,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Removed...'.red.inverse);
     process.exit();
   } catch (err) {
