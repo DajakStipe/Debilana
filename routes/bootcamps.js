8299d76915efffb1542ac6b059ecac725e6c8ca7
DajakStipe <dajakstipe@gmail.com>
2020-06-17 16:59:28 +0200

    Added filtering as a middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,6 +11,10 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+// middleware
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource Routers
 const courseRouter = require('./courses');
 
@@ -24,7 +28,10 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
