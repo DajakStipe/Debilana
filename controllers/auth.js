0554dac1bccf1cae78bd114176ba29df42eb3129
DajakStipe <dajakstipe@gmail.com>
2020-06-18 18:51:06 +0200

    Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,5 +19,36 @@ exports.register = asyncHandler(async (req, res, next) => {
   // CREATE TOKEN
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token: token });
+  res.status(200).json({ success: true, token });
+});
+
+// @desc      LOGIN User
+// @route     post /api/v1/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
+
+  // check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
+
+  // CREATE TOKEN
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
