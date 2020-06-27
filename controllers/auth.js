0e38a824f06c5dbc264efd5463a645801c614d99
DajakStipe <dajakstipe@gmail.com>
2020-06-27 10:45:40 +0200

    Update user details (email,name,password)

--- controllers/auth.js
+++ controllers/auth.js
@@ -158,3 +158,39 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
+
+// @desc      Update User details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Public
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatePassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is not correct', 401));
+  }
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
