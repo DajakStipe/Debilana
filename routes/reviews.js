5b880269ddcf1d93d0b5ed9e659f48c8ad267305
DajakStipe <dajakstipe@gmail.com>
2020-07-01 14:37:33 +0200

    Full CRUD for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 // importing methods from controller
-const { getReviews, getReview, addReview } = require('../controllers/reviews');
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview
+} = require('../controllers/reviews');
 
 const Course = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
@@ -21,6 +27,10 @@ router
   )
   .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
